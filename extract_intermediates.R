339889b002b6164db5450ca55d3bbe5efd1b788f
Marissa Lee <marissaruthlee@gmail.com>
2018-05-27 14:47:09 -0400

    adds boral diagnostics figure

--- extract_intermediates.R
+++ extract_intermediates.R
@@ -1,11 +1,23 @@
+#load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(gridExtra)
+library(GGally)
+library(boral) #NOTE---- boral uses JAGS so you need to first install from http://sourceforge.net/projects/mcmc-jags/files/JAGS/
+
+#load functions
 source('code/boral_modfit.R')
+source('code/boral_diagnostics.R')
+source('code/load_data.R')
 
 # extract minimal intermediates from boral fit objects and save as .RData files
 test.select <- FALSE
-extract_TraitLVs_allXs.mcmc.obj(test.select) # didn't upload this intermediate because its too big (>2GB), so code to produce the downstream figure is below
+#extract_TraitLVs_allXs.mcmc.obj(test.select) # don't upload this intermediate because its too big (>2GB)... see below
 extract_TraitLVs_allXs.Xcoefs.df(test.select)
 extract_TraitLVs_allXs.cor.df(test.select)
 extract_TraitLVs_selectXs.cor.df(test.select)
+
 
 # boral_diagnostics.R : Report fit diagnostics
 TraitLVs_allXs.mcmc.obj <- readRDS("derived_data/boralFits/Traits-and-LVs_allXs_allruns_mcmcObjreal.RData")
